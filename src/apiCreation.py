71510977a2ca924b6768765e78f741cb29358817
breogann <fernandocostapradillo@gmail.com>
2020-05-06 20:20:36 +0200

    improved interface

--- src/apiCreation.py
+++ src/apiCreation.py
@@ -39,22 +39,53 @@ def upload_file():
             to = filename
             return redirect(to)
     return '''
-     <center>
-    <!doctype html>
+<!DOCTYPE html>
+<html lang=“en”>
+<center>
+<head>
+    <meta charset=“UTF-8">
+    <meta name=“viewport” content=“width=device-width, initial-scale=1.0">
     <title>Neural network tells accents apart</title>
-    <h1>Which is my accent? 🎙</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=⬆️UPLOAD>
+</head>
+<body style="background-color:#946B9C;">
+<h1>Which is my accent? 🎙</h1>
+<h2>Record yourself reading the following text and upload it so the cat can judge you:</h2>
+<p style="text-align:left;"> 🛠 If you're using macOS, open the Voice memo app and drag your voice note to your desktop.</p>
+<p>"Please call Stella.  Ask her to bring these things with her from the store:  Six spoons of fresh snow peas, five thick slabs of blue cheese, and maybe a snack for her brother Bob.  We also need a small plastic snake and a big toy frog for the kids.  She can scoop these things into three red bags, and we will go meet her Wednesday at the train station."</p>
+<form method=post enctype=multipart/form-data>
+      <input type=file name=file style="height:40px; width:80px">
+      <input type=submit value=⬆️UPLOAD style="height:40px; width:80px">
     </form>
-    </center>
-    '''
+<img src="https://media1.tenor.com/images/0291ad4acb10e5d76bf617e255d130b4/tenor.gif?itemid=12986754"/>
+</body>
+<center>
+</html>
+'''
 
 @app.route('/<audioname>', methods=['GET'])
 def accent (audioname):
     path = f"../input/{audioname}"
     gendf = generateDataframe(path)
-    result = runModel(gendf) #To predict 
-    return result
+    result = runModel(gendf) #To predict  
+    return '''
+<!DOCTYPE html>
+<html lang=“en”>
+<center>
+<head>
+    <meta charset=“UTF-8">
+    <meta name=“viewport” content=“width=device-width, initial-scale=1.0">
+    <title>Neural network tells accents apart</title>
+</head>
+<body style="background-color:#946B9C;">
+<h1>Which is my accent? 🎙</h1>
+<img src="https://media1.tenor.com/images/0291ad4acb10e5d76bf617e255d130b4/tenor.gif?itemid=12986754"/>
+<h2>{result}</h2>
+<form>
+    <input type="button" value="Go back" onclick="history.back()">
+</form> 
+</body>
+<center>
+</html>
+'''.format(result=result)
 
 app.run('0.0.0.0', port=3000, debug=False, threaded=False) #http://0.0.0.0:3000/
